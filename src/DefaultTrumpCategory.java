986cf9259003c5435c1ae580424a8531b95c7920
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2016-08-16 23:13:26 +1000

    getName() passed unit test

--- src/DefaultTrumpCategory.java
+++ src/DefaultTrumpCategory.java
@@ -2,11 +2,14 @@
  * Created by calebmacdonaldblack on 16/08/2016.
  */
 public class DefaultTrumpCategory implements TrumpCategory {
+    private String name;
+
     public DefaultTrumpCategory(String name) {
+        this.name = name;
     }
 
     @Override
     public String getName() {
-        return null;
+        return this.name;
     }
 }
