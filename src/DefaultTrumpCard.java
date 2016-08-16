620b78d9bb42513be0f6d6e025d830468449c482
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2016-08-16 23:23:02 +1000

    added passing production code for DefaultTrumpCard

--- src/DefaultTrumpCard.java
+++ src/DefaultTrumpCard.java
@@ -2,12 +2,15 @@
  * Created by calebmacdonaldblack on 16/08/2016.
  */
 public class DefaultTrumpCard extends DefaultCard implements TrumpCard {
+    private TrumpCategory trumpCategory;
+
     public DefaultTrumpCard(String heading, String subHeading, TrumpCategory trumpCategory) {
         super(heading, subHeading);
+        this.trumpCategory = trumpCategory;
     }
 
     @Override
     public TrumpCategory getTrumpCategory() {
-        return null;
+        return this.trumpCategory;
     }
 }
