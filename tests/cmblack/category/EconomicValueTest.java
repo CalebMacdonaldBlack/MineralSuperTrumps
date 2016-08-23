3b77bc26e3e331e7bb866f137e5cceb415b1a194
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2016-08-23 19:36:09 +1000

    calling correct method now

--- tests/cmblack/category/EconomicValueTest.java
+++ tests/cmblack/category/EconomicValueTest.java
@@ -22,6 +22,6 @@ public class EconomicValueTest {
 
     @Test
     public void testGetName1() throws Exception {
-        assertEquals("eco val", new EconomicValue("eco val" ,EconomicValue.EconomicValueOptions.HIGH));
+        assertEquals("eco val", new EconomicValue("eco val" ,EconomicValue.EconomicValueOptions.HIGH).getName());
     }
 }
