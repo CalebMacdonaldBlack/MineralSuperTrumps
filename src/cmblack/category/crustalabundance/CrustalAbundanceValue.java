c9a172eee9cbb8801666197804f353286c61a408
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2016-09-10 09:41:41 +1000

    added CrustalAbundance Enum

---
+++ src/cmblack/category/crustalabundance/CrustalAbundanceValue.java
@@ -0,0 +1,23 @@
+package cmblack.category.crustalabundance;
+
+/**
+ * Created by calebmacdonaldblack on 10/09/2016.
+ */
+public enum CrustalAbundanceValue {
+    ULTRATRACE("ultratrace"),
+    TRACE("trace"),
+    LOW("low"),
+    MODERATE("moderate"),
+    HIGH("high"),
+    VERY_HIGH("very high");
+
+    private final String text;
+
+    CrustalAbundanceValue(String text) {
+        this.text = text;
+    }
+
+    public String getText(){
+        return this.text;
+    }
+}
