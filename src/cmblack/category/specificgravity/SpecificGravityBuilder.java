1d32a11f738b044327d98e3efea5ad76bd9e0f28
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2016-09-10 13:14:37 +1000

    added implementation for SpecificGravityBuilder

--- src/cmblack/category/specificgravity/SpecificGravityBuilder.java
+++ src/cmblack/category/specificgravity/SpecificGravityBuilder.java
@@ -1,16 +1,35 @@
 package cmblack.category.specificgravity;
 
+import cmblack.category.Category;
+import cmblack.category.ICategory;
+
 /**
  * Created by calebmacdonaldblack on 10/09/2016.
  */
 public class SpecificGravityBuilder implements ISpecificGravityBuilder {
     @Override
     public ISpecificGravity build(String value, String categoryName) {
-        return null;
+        ICategory category = new Category(categoryName);
+        return new SpecificGravity(
+                this.getLowFromRangeString(value),
+                this.getHighFromRangeString(value),
+                category
+                );
     }
 
     @Override
     public ISpecificGravity build(String value) {
-        return null;
+        return this.build(value, "Specific gravity");
+    }
+
+    //TODO duplicate code: remove this
+    private Double getLowFromRangeString(String doubleRange) {
+        return Double.parseDouble(doubleRange.split("-")[0]);
+    }
+
+    //TODO duplicate code: remove this
+    private Double getHighFromRangeString(String doubleRange) {
+        String[] splitDouble = doubleRange.split("-");
+        return splitDouble.length > 1 ? Double.parseDouble(splitDouble[1]) : Double.parseDouble(splitDouble[0]);
     }
 }
