d90d049b21cf72fe8930b94851a4304859e69f55
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2016-09-10 12:45:16 +1000

    added implementation for CrustalAbundanceBuilder

--- src/cmblack/category/crustalabundance/CrustalAbundanceBuilder.java
+++ src/cmblack/category/crustalabundance/CrustalAbundanceBuilder.java
@@ -1,12 +1,31 @@
 package cmblack.category.crustalabundance;
 
+import cmblack.category.Category;
+import cmblack.category.ICategory;
+
 /**
  * Created by calebmacdonaldblack on 10/09/2016.
  */
 public class CrustalAbundanceBuilder implements ICrustalAbundanceBuilder {
     @Override
     public ICrustalAbundance build(String value, String categoryName) {
-        return null;
+        ICategory category = new Category(categoryName);
+        switch (value){
+            case "ultratrace":
+                return new CrustalAbundance(category, CrustalAbundanceValue.ULTRATRACE);
+            case "trace":
+                return new CrustalAbundance(category, CrustalAbundanceValue.TRACE);
+            case "low":
+                return new CrustalAbundance(category, CrustalAbundanceValue.LOW);
+            case "moderate":
+                return new CrustalAbundance(category, CrustalAbundanceValue.MODERATE);
+            case "high":
+                return new CrustalAbundance(category, CrustalAbundanceValue.HIGH);
+            case "very high":C
+                return new CrustalAbundance(category, CrustalAbundanceValue.VERY_HIGH);
+            default:
+                throw new NullPointerException("CrustalAbundance getValue for " + value + " is not found");
+        }
     }
 
     @Override
