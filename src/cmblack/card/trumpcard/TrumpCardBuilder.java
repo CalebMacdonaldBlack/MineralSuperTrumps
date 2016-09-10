5638a856ab27fe6cc68717801f6b49335ad848c5
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2016-09-10 13:57:02 +1000

    added test for new TrumpCardBuilder implementation

---
+++ src/cmblack/card/trumpcard/TrumpCardBuilder.java
@@ -0,0 +1,13 @@
+package cmblack.card.trumpcard;
+
+import cmblack.deck.IParsedCard;
+
+/**
+ * Created by calebmacdonaldblack on 10/09/2016.
+ */
+public class TrumpCardBuilder implements ITrumpCardBuilder {
+    @Override
+    public ITrumpCard build(IParsedCard parsedCard) {
+        return null;
+    }
+}
---
+++ tests/cmblack/card/trumpcard/TrumpCardBuilderTest.java
@@ -0,0 +1,26 @@
+package cmblack.card.trumpcard;
+
+import cmblack.deck.IParsedCard;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by calebmacdonaldblack on 10/09/2016.
+ */
+public class TrumpCardBuilderTest {
+
+    ITrumpCardBuilder trumpCardBuilder = new TrumpCardBuilder();
+    IParsedCard parsedCard = new IParsedCard.FakeParsedCard2();
+
+    @Test
+    public void testBuild() throws Exception {
+        ITrumpCard trumpCard = trumpCardBuilder.build(parsedCard);
+        assertEquals("The Geophysist", trumpCard.getTitle());
+        assertEquals("Changed the trump category to Specific gravity", trumpCard.getSubTitle());
+        assertTrue(Arrays.equals(trumpCard.getCategories(), new String[]{"Specific gravity"}));
+        assertEquals("slide59.jpg", trumpCard.getImageFilename());
+    }
+}
