c8e7115ddf5d9d44c9541b6914876b41e51ec020
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2016-09-10 15:42:17 +1000

    added Player interface

---
+++ src/cmblack/IPlayer.java
@@ -0,0 +1,31 @@
+package cmblack;
+
+import cmblack.card.ICard;
+import cmblack.card.trumpcard.ITrumpCard;
+import cmblack.category.Category;
+
+import java.util.ArrayList;
+
+/**
+ * Created by calebmacdonaldblack on 10/09/2016.
+ */
+public interface IPlayer {
+    IPlayer addCard(ICard card);
+    ICard playCard(ICard cardToBeat, Category currentTrumpCategory);
+
+    class FakePlayer implements IPlayer {
+
+        private ArrayList<ICard> cards;
+
+        @Override
+        public IPlayer addCard(ICard card) {
+            this.cards.add(card);
+            return this;
+        }
+
+        @Override
+        public ICard playCard(ICard cardToBeat, Category currentTrumpCategory) {
+            return new ITrumpCard.FakeTrumpCard();
+        }
+    }
+}
