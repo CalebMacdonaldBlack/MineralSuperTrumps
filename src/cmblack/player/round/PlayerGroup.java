5ba0a6f6a21cce3abd51917e7d475b699d38c51f
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2016-09-12 08:23:03 +1000

    implemented playerGroup

--- src/cmblack/player/round/PlayerGroup.java
+++ src/cmblack/player/round/PlayerGroup.java
@@ -1,5 +1,6 @@
 package cmblack.player.round;
 
+import cmblack.player.EmptyPlayer;
 import cmblack.player.IPlayer;
 
 import java.util.ArrayList;
@@ -9,32 +10,57 @@ import java.util.ArrayList;
  */
 public class PlayerGroup implements IPlayerGroup {
 
-    private final ArrayList<IPlayer> remainingPlayers = new ArrayList<>();
+    private final ArrayList<IPlayer> remainingPlayers;
     private final ArrayList<IPlayer> removedPlayers = new ArrayList<>();
     private final IPlayer[] players;
 
     public PlayerGroup(IPlayer[] players) {
         this.players = players;
+        this.remainingPlayers = playersAsArrayList(players);
+    }
+
+    private ArrayList<IPlayer>  playersAsArrayList(IPlayer[] players) {
+        ArrayList<IPlayer> playersArrayList = new ArrayList<>();
+
+        for(IPlayer player: players){
+            playersArrayList.add(player);
+        }
+        return playersArrayList;
     }
 
 
     @Override
-    public IPlayer getNextPlayer(IPlayer player) {
-        return null;
+    public IPlayer getNextPlayer(IPlayer currentPlayer) {
+        if(currentPlayer.equals(new EmptyPlayer())){
+            return remainingPlayers.get(0);
+        }
+
+        for(int i=0;i<remainingPlayers.size();i++){
+            if(remainingPlayers.get(i).equals(currentPlayer)){
+                if(i == remainingPlayers.size() -1){
+                    return remainingPlayers.get(0);
+                }else{
+                    return remainingPlayers.get(i + 1);
+                }
+            }
+        }
+        throw new NullPointerException(currentPlayer.getName() + " cound not be found in the players in round");
     }
 
     @Override
     public IPlayer removePlayer(IPlayer player) {
-        return null;
+        this.remainingPlayers.remove(player);
+        this.removedPlayers.add(player);
+        return player;
     }
 
     @Override
     public ArrayList<IPlayer> getRemainingPlayers() {
-        return null;
+        return this.remainingPlayers;
     }
 
     @Override
     public ArrayList<IPlayer> getRemovedPlayers() {
-        return null;
+        return this.removedPlayers;
     }
 }
