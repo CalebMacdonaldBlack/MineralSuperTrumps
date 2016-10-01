5379437ee6e43d66deb1eafe68875abe9eba5399
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2016-10-01 12:09:19 +1000

    fixed winners names not showing up at the end

--- src/App/Views/GameView.java
+++ src/App/Views/GameView.java
@@ -22,16 +22,16 @@ public class GameView {
         for(int i=0;i<winners.size();i++){
             switch (i){
                 case 0:
-                    System.out.println("1st: " + winners.get(i));
+                    System.out.println("1st: " + winners.get(i).getName());
                     break;
                 case 1:
-                    System.out.println("2nd: " + winners.get(i));
+                    System.out.println("2nd: " + winners.get(i).getName());
                     break;
                 case 2:
-                    System.out.println("3rd: " + winners.get(i));
+                    System.out.println("3rd: " + winners.get(i).getName());
                     break;
                 default:
-                    System.out.println((i+1) + "th: " + winners.get(i));
+                    System.out.println((i+1) + "th: " + winners.get(i).getName());
             }
         }
     }
